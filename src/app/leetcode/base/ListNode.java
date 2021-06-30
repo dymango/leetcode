b5b375e8ed4137150e1e4f207d84be68a59e5610
Dimmy Yang <dimmyyang@chancetop.com>
2021-06-30 14:14:14 +0800

    review: LIST NODE BFS\DP

---
+++ src/app/leetcode/base/ListNode.java
@@ -0,0 +1,21 @@
+package app.leetcode.base;
+
+/**
+ * @author dimmy
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
