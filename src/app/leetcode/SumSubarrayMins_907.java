01b934a5d437107bbeb049fbbf534fd36e0e674d
Dimmy Yang <dimmyyang@chancetop.com>
2023-02-17 17:52:46 +0800

    907/908/909/910/911

--- src/app/leetcode/SumSubarrayMins_907.java
+++ src/app/leetcode/SumSubarrayMins_907.java
@@ -71,29 +71,41 @@ public class SumSubarrayMins_907 {
     public int sumSubarrayMinsV2(int[] arr) {
         long sum = 0;
         Stack<Integer> indexStack = new Stack<>();
-        Stack<Integer> valueStack = new Stack<>();
         for (int i = 0; i <= arr.length; i++) {
             int n = i == arr.length ? 0 : arr[i];
             if (indexStack.isEmpty()) {
                 indexStack.push(i);
-                valueStack.push(n);
                 continue;
             }
 
-            while (!valueStack.isEmpty() && valueStack.peek() > n) {
-                Integer tv = valueStack.pop();
+            while (!indexStack.isEmpty() && arr[indexStack.peek()] > n) {
                 Integer ti = indexStack.pop();
-
                 int left = indexStack.isEmpty() ? -1 : indexStack.peek();
-                sum += (long) (ti - left) * (i - ti) * tv;
+                sum += (long) (ti - left) * (i - ti) * arr[ti];
                 sum %= 1000000007;
             }
 
             indexStack.push(i);
-            valueStack.push(n);
         }
 
         return (int) sum;
+    }
+
+    public int sumSubarrayMinsV3(int[] arr) {
+        long result = 0;
+        int[] stack = new int[arr.length]; // 使用数组结构模拟堆栈，里面存储arr数组的下标，为了便于计算“管辖区域”的跨度
+        int head = 0, tail = head, mod = (int) (1e9 + 7); // 配合模拟堆栈的head指针和tail指针
+        for (int i = 0; i <= arr.length; i++) {
+            int num = (i == arr.length) ? 0 : arr[i]; // 如果arr数组遍历到最后一个元素，则还需要模拟结尾元素0，为了让stack中元素都出栈
+            while (head != tail && arr[stack[tail - 1]] > num) {
+                int n = stack[--tail]; // 待计算数字arr[n]的【数组下标】
+                int h = (head != tail) ? stack[tail - 1] : -1; // arr[n]的“辐射区域”head头的【数组下标】（开区间）
+                int t = i; // arr[n]的“辐射区域”tail尾的【数组下标】（开区间）
+                result = (result + (long) (n - h) * (t - n) * arr[n]) % mod;
+            }
+            stack[tail++] = i;
+        }
+        return (int) result;
     }
 
     public static void main(String[] args) {
