0df638fe361778013d526b19011766ee2791aca3
Dimmy Yang <dimmyyang@chancetop.com>
2023-05-06 17:05:04 +0800

    bulk resolve

--- src/app/tencent/ContainsDuplicate_217.java
+++ src/app/tencent/ContainsDuplicate_217.java
@@ -35,8 +35,8 @@ public class ContainsDuplicate_217 {
      */
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> set = new HashSet<>();
-        for (int i = 0; i < nums.length; i++) {
-            if(!set.add(nums[i])) return true;
+        for (int num : nums) {
+            if (!set.add(num)) return true;
         }
 
         return false;
